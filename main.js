71295dd2210d3ae5db8a53fc19068842d2c8b19a
Dario <hz-hertzio@hotmail.com>
2020-04-27 22:22:01 -0500

    rosweb

--- main.js
+++ main.js
@@ -6,32 +6,82 @@ var app = new Vue({
         ros: null,
         ws_address: 'ws://192.168.154.130:9090',
         logs: [],
+        loading:null,
+        topic: null,
+        message: null,
     },
     // helper methods to connect to ROS
     methods: {
         connect: function() {
-            this.logs.unshift('connect to rosbridge server!!')
-            console.log('connect to rosbridge server!!')
+            this.loading = true
             this.ros = new ROSLIB.Ros({
                 url: this.ws_address
             })
             this.ros.on('connection', () => {
                 this.connected = true
-                this.logs.unshift('Connected!')
-                console.log('Connected!')
+                this.logs.unshift((new Date()).toTimeString()+'-Connected!')
+                this.loading = false
             })
             this.ros.on('error', (error) => {
-                this.logs.unshift('Error connecting to websocket server')
-                console.log('Error connecting to websocket server: ', error)
+                this.logs.unshift((new Date()).toTimeString()+'-Error: ${error} ')
             })
             this.ros.on('close', () => {
                 this.connected = false
-                this.logs.unshift('Connection to websocker server closed')
-                console.log('Connection to websocket server closed.')
+                this.logs.unshift((new Date()).toTimeString()+'-Disconnected')
+                this.loading = false
             })
         },
+
         disconnect: function(){
             this.ros.close()
-        }
+        },
+
+        setTopic: function() {
+            this.topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+        },
+        forward: function() {
+            this.message = new ROSLIB.Message({
+                linear: { x: 1, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+        stop: function() {
+            this.message = new ROSLIB.Message({
+                linear: { x: 0, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+        backward: function() {
+            this.message = new ROSLIB.Message({
+                linear: { x: -1, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+        turnLeft: function() {
+            this.message = new ROSLIB.Message({
+                linear: { x: 0.5, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0.5, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+        turnRight: function() {
+            this.message = new ROSLIB.Message({
+                linear: { x: 0.5, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: -0.5, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
     },
 })
